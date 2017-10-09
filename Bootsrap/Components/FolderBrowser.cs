5b2c4bd2adcf46c02c0b95555a59484f76244451
Brady Kelly <brady@bradykelly.net>
2017-10-09 08:45:17 +0200

    Component code in place.

---
+++ Bootsrap/Components/FolderBrowser.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Bootstrap.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+
+namespace Bootstrap.Components
+{
+    /// <summary>
+    /// Code-behind for the <b>FolderBrowser</b> view component.
+    /// </summary>
+    public class FolderBrowser: ViewComponent
+    {
+        private IOptions<FileTreeConfig> _config;
+
+        public FolderBrowser(IOptions<FileTreeConfig> config)
+        {
+            _config = config;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string dir, bool showFiles = false)
+        {
+            var browsingRoot = Path.Combine(_config.Value.BaseDir, dir);
+            var nodes = new List<TreeNode>();
+            nodes.AddRange(RecurseDirectory(browsingRoot));
+            return new ContentViewComponentResult(JsonConvert.SerializeObject(nodes));
+        }
+
+        private List<TreeNode> RecurseDirectory(string directory)
+        {
+            var ret = new List<TreeNode>();
+            var dirInfo = new DirectoryInfo(directory);
+
+            try
+            {
+                var directories = dirInfo.GetDirectories("*", SearchOption.TopDirectoryOnly);
+                foreach (var dir in directories)
+                {
+                    if (dir.FullName.ToLower() == dirInfo.FullName)
+                    {
+                        continue;
+                    }
+                    var thisNode = TreeNode.FromDirInfo(dir);
+                    thisNode.Nodes.AddRange(RecurseDirectory(dir.FullName));
+                    ret.Add(thisNode);
+                }
+            }
+            catch (UnauthorizedAccessException ux)
+            {
+                // NB Log.
+            }
+
+            return ret;
+        }
+    }
+}
